72c207144e05668b9433e8d91a29e4c09086623a
analaurafr <laurafernandes@outlook.com>
2024-03-12 16:16:51 -0300

    test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,38 +2,5 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    # Caminho do arquivo de teste
-    test_file_path = "data/test_jobs.csv"
-
-    # Palavra a ser contabilizada
-    test_word = "Python"
-
-    # Conteúdo do arquivo de teste
-    test_data = (
-        "Python is a programming language. "
-        "javascript is another language. "
-        "Python, JavaScript, python."
-    )
-
-    # Escreva o conteúdo no arquivo de teste
-    with open(test_file_path, "w") as test_file:
-        test_file.write(test_data)
-
-    # Chame a função count_ocurrences
-    result = count_ocurrences(test_file_path, test_word)
-
-    # Verifique se a contagem está correta
-    assert result == 3
-
-    # Teste de case-insensitivity
-    result_case_insensitive = count_ocurrences(test_file_path, "python")
-    assert result_case_insensitive == 3
-
-    # Teste com palavra que não existe
-    result_nonexistent_word = count_ocurrences(test_file_path, "Java")
-    assert result_nonexistent_word == 0
-
-    # Limpeza do arquivo de teste
-    import os
-
-    os.remove(test_file_path)
+    assert count_ocurrences("data/jobs.csv", "Python") == 1639
+    assert count_ocurrences("data/jobs.csv", "JavaScript") == 122
