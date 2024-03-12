ab21f59dfc168f61e7f91152129042b5ba6407e4
analaurafr <laurafernandes@outlook.com>
2024-03-12 16:29:19 -0300

    jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            read_data = csv.DictReader(file, delimiter=",")
+            for row in read_data:
+                if row:
+                    self.jobs_list.append(row)
 
     def get_unique_job_types(self) -> List[str]:
         pass
